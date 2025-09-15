dcc923d48f6dbc8e44afd2bbeaa07c3d99be2d50
Gerasim Troeglazov <3dEyes@gmail.com>
2025-09-15 11:26:51 +1000

    Cleanup constants

--- SVGApplication.h
+++ SVGApplication.h
@@ -22,20 +22,8 @@
 
 #include <private/interface/WindowStack.h>
 
+#include "SVGConstants.h"
 #include "SVGMainWindow.h"
-
-#define APP_SIGNATURE "application/x-vnd.svgear"
-#define STL_SIGNATURE "image/svg+xml"
-
-#define APP_SETTINGS_FILENAME "SVGear_settings"
-
-#define URL_HOMEPAGE		"https://github.com/threedeyes/SVGear"
-#define URL_HOMEPAGE_WIKI	"https://github.com/threedeyes/SVGear/wiki"
-
-#define MSG_APP_QUIT					'QAPP'
-#define MSG_WINDOW_ACTIVATED			'AWIN'
-#define MSG_WINDOW_CLOSED				'CWIN'
-#define MSG_EASTER_EGG					'EEGG'
 
 class SVGMainWindow;
 
