df551f4f344863728cd192e392cb5b69558570da
Gerasim Troeglazov <3dEyes@gmail.com>
2026-01-01 13:51:58 +1000

    Increase page limit size

--- Dialogs/HVIF-Store/HvifStoreDefs.h
+++ Dialogs/HVIF-Store/HvifStoreDefs.h
@@ -10,7 +10,7 @@
 
 #define SERVER_URL "https://hvif-store.art"
 
-static const int32 kDefaultPageLimit = 24;
+static const int32 kDefaultPageLimit = 40;
 static const bigtime_t kSearchDebounceDelay = 400000;
 static const float kBaseFontSize = 12.0f;
 static const float kBaseWindowWidth = 860.0f;
